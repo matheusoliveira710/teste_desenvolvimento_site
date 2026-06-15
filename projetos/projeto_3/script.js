edd138f55d547678102b923b5ae5e303afe54a13
Matheus Oliveira <matheuscorreia2002@gmail.com>
2026-06-15 10:34:57 -0300

    Adicionado função de converter moedas!

--- projetos/projeto_3/script.js
+++ projetos/projeto_3/script.js
@@ -1,5 +1,4 @@
 // aqui vai a API de conversão de moedas
-
 function converter() {
     const valor = parseFloat(document.getElementById('valor').value);
     const moeda = document.getElementById('moeda').value;
@@ -8,6 +7,19 @@ function converter() {
         resultado.textContent = "Por favor, digite um valor válido em reais.";
         return;
     }
+    if (moeda === "") {
+        resultado.textContent = "Erro, por favor selecione uma moeda!";
+        return;
+    }
+    fetch("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_0EnEK29NWnAECwYPtHdGqF3i1n6NGaWbd31TBx8t")
+        .then(response => response.json())
+        .then(data => {
+            console.log("API funcionando!");
+            const cotacao = data.data[moeda];
+            console.log(data);
+            console.log("Moeda:", moeda);
+            console.log("Cotação:", cotacao);
+        });
     //parsefloat("")
-    resultado.textContent = valor + " " + moeda;
+    resultado.textContent = `Convertendo R$${valor} para ${moeda}`;
 }
