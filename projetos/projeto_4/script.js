a2b537ddc67ed9aac2cdf244b0e3414046ff4898
Matheus Oliveira <matheuscorreia2002@gmail.com>
2026-07-05 21:31:20 -0300

    Adicionado escolha de horário, nome, telefone e email! ao formulário

---
+++ projetos/projeto_4/script.js
@@ -0,0 +1,4 @@
+const nome = document.getElementById('nome').value;
+const telefone = document.getElementById('telefone').value;
+const email = document.getElementById('email').value;
+const horario = document.getElementById('horario').value;
